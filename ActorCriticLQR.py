847b1725fc4c1d8028d4bb612f70d828da84f0a2
MoZhou1995 <31834863+MoZhou1995@users.noreply.github.com>
2022-02-01 09:14:17 -0500

    Add files via upload

--- ActorCriticLQR.py
+++ ActorCriticLQR.py
@@ -11,11 +11,12 @@ from absl import flags
 import numpy as np
 # import matplotlib.pyplot as plt
 
-flags.DEFINE_string('config_path', 'cfg2.json',
+flags.DEFINE_string('config_path', 'example1.json',
                     """The path to load json file.""")
 flags.DEFINE_string('exp_name', None,
                     """The name of numerical experiments, prefix for logging""")
 FLAGS = flags.FLAGS
+num_run = 10
 
 def main(argv):
     del argv
@@ -30,103 +31,135 @@ def main(argv):
     R = np.array(config['R'])
     D_xi = np.array(config['D_xi'])
     sigma = config['sigma']
-    alpha = config['alpha']
-    beta = config['beta']
-    T = config['T']
+    # alpha = config['alpha']
+    # beta = config['beta']
+    # T = config['T']
     N = config['N']
     N0 = config['N0']
     N1 = config['N1']
-    log_freq = config['log_freq']
+    # log_freq = config['log_freq']
     D_ep = D_xi + sigma**2 * np.matmul(B, np.transpose(B))
     d, k = np.shape(B)
-    
-    # initialization
-    theta = np.zeros([d+k, d+k])
-    K = np.zeros([k, d])
-    train_history = []
-    
-    def solve(init, A_BK, d):
-        # matrix equation: ans = init + A_BK * ans * A_BK^T, d is the dim
-        tens = np.kron(A_BK, A_BK)
-        ans_vec = np.linalg.solve(np.identity(d**2)-tens, np.matrix.flatten(init))
-        return np.reshape(ans_vec, [d,d])
-    
-    D_ep = D_xi + sigma**2 * np.matmul(B, np.transpose(B))
-    P_K_star = dscrt_rct(A,B,Q,R)
-    K_star = np.linalg.solve(R+np.linalg.multi_dot([np.transpose(B),P_K_star,B]), np.linalg.multi_dot([np.transpose(B),P_K_star,A]))
-    J_K_star = np.trace(np.matmul(D_ep, P_K_star))
-    
-    max_norm = [0, 0, 0, 0] # record the max norm, for assump 1
-    
-    # training process
-    for t in range(T+1):
-        A_BK = A - np.matmul(B,K) # dxd
-        # compute and record the errors 
-        if t % log_freq == 0:
-            P_K = solve(Q+np.linalg.multi_dot([np.transpose(K),R,K]) , np.transpose(A_BK), d)
-            # D_K = solve(D_ep , A_BK, d)
-            temp1 = np.concatenate((Q+np.linalg.multi_dot([np.transpose(A),P_K,A]), np.linalg.multi_dot([np.transpose(A),P_K,B])), axis = 1)
-            temp2 = np.concatenate((np.linalg.multi_dot([np.transpose(B),P_K,A]), R+ np.linalg.multi_dot([np.transpose(B),P_K,B])), axis = 1)
-            theta_K = np.concatenate((temp1, temp2), axis=0)
-            rel_err_theta = np.linalg.norm(theta - theta_K)**2
-            J_K = np.trace(np.matmul(D_ep, P_K))
-            err_actor = J_K - J_K_star
-            err_K = np.linalg.norm(K - K_star)
-            train_history.append([t, rel_err_theta, err_actor, err_K])
-            print("errors", rel_err_theta, err_actor, err_K)
+   
+    #run the algorithm num_run times
+    max_norm = np.zeros(5)
+    for size in range(6):
+        print('size ', size)
+        factor = 2**size
+        T = 125 * factor
+        log_freq = 5 * factor
+        alpha = 0.032 / factor
+        beta = 0.032 / factor
+        for index in range(num_run):
+            print('round ', index+1, ' start')
+            # initialization
+            theta = np.zeros([d+k, d+k])
+            K = np.zeros([k, d])
+            train_history = []
+            
+            def solve(init, A_BK, d):
+                # matrix equation: ans = init + A_BK * ans * A_BK^T, d is the dim
+                tens = np.kron(A_BK, A_BK)
+                ans_vec = np.linalg.solve(np.identity(d**2)-tens, np.matrix.flatten(init))
+                return np.reshape(ans_vec, [d,d])
+            
+            D_ep = D_xi + sigma**2 * np.matmul(B, np.transpose(B))
+            P_K_star = dscrt_rct(A,B,Q,R)
+            K_star = np.linalg.solve(R+np.linalg.multi_dot([np.transpose(B),P_K_star,B]), np.linalg.multi_dot([np.transpose(B),P_K_star,A]))
+            J_K_star = np.trace(np.matmul(D_ep, P_K_star))
+            
+            # max_norm = [0, 0, 0, 0] # record the max norm, for assump 1
+            
+            # training process
+            for t in range(T+1):
+                A_BK = A - np.matmul(B,K) # dxd
+                # compute and record the errors 
+                if t % log_freq == 0:
+                    P_K = solve(Q+np.linalg.multi_dot([np.transpose(K),R,K]) , np.transpose(A_BK), d)
+                    # D_K = solve(D_ep , A_BK, d)
+                    temp1 = np.concatenate((Q+np.linalg.multi_dot([np.transpose(A),P_K,A]), np.linalg.multi_dot([np.transpose(A),P_K,B])), axis = 1)
+                    temp2 = np.concatenate((np.linalg.multi_dot([np.transpose(B),P_K,A]), R+ np.linalg.multi_dot([np.transpose(B),P_K,B])), axis = 1)
+                    theta_K = np.concatenate((temp1, temp2), axis=0)
+                    rel_err_theta = np.linalg.norm(theta - theta_K)**2
+                    J_K = np.trace(np.matmul(D_ep, P_K))
+                    err_actor = J_K - J_K_star
+                    err_K = np.linalg.norm(K - K_star)
+                    train_history.append([t, rel_err_theta, err_actor, err_K])
+                    # print("errors", rel_err_theta, err_actor, err_K)
+                
+                # compute the max norms in order to check assumption 1
+                rho_A_BK = np.max(abs(np.linalg.eigvals(A_BK)))
+                norm_A_BK = np.linalg.norm(A_BK, ord=2)
+                E = np.matmul(np.concatenate((np.identity(d),-K), axis=0), np.concatenate((A,B), axis=1))
+                norm_E = np.linalg.norm(E, ord=2)
+                norm_K = np.linalg.norm(K, ord=2)
+                norm_theta = np.linalg.norm(theta)
+                max_norm = np.maximum(max_norm, [rho_A_BK, norm_A_BK, norm_E, norm_K, norm_theta])
+                
+                # critic
+                # observe N0 trajectories
+                x_N0 = np.zeros([N, d]) #Nxd
+                for _ in range(N0):
+                    x_N0 = np.tensordot(x_N0, A_BK, axes=([1],[1]))
+                    x_N0 += np.reshape(normal.rvs(mean=np.zeros([d]), cov=D_ep, size=(N)), [N,d])
+                u_N0 = - np.tensordot(x_N0, K, axes=([1],[1])) + sigma**2 * np.reshape(normal.rvs(size=[N,k]), [N,k]) #Nxk
+                xu_N0 = np.concatenate((x_N0, u_N0), axis=1)
+                
+                # observe hat_psi_j
+                x_prime = np.tile(np.tensordot(x_N0, A, axes=([1],[1])), (N1,1,1)) #N1xNxd
+                x_prime += np.tile(np.tensordot(u_N0, B, axes=([1],[1])), (N1,1,1)) #N1xNxd
+                x_prime += np.reshape(normal.rvs(mean=np.zeros([d]), cov=D_xi, size=(N1, N)), [N1,N,d]) #N1xNxd
+                u_prime = - np.tensordot(x_prime, K, axes=([2],[1])) + sigma**2 * np.reshape(normal.rvs(size=[N1, N, k]), [N1, N, k])#N1xNxk
+                xu_prime = np.concatenate((x_prime, u_prime), axis=2) #N1xNx(d+k)
+                phi_N0 = np.reshape(xu_N0, [N,d+k,1]) * np.reshape(xu_N0, [N,1,d+k])#Nx(d+k)x(d+k)
+                psi = np.reshape(xu_prime, [N1,N,d+k,1]) * np.reshape(xu_prime, [N1,N,1,d+k]) - np.reshape(phi_N0, [1,N,d+k,d+k]) #N1xNx(d+k)x(d+k)
+                
+                # compute critic gradient
+                bar_psi = np.mean(psi, axis=0, keepdims=True) #1xNx(d+k)x(d+k)
+                c_N0 = np.sum(np.matmul(x_N0, Q) * x_N0, axis=1, keepdims = True)\
+                    + np.sum(np.matmul(u_N0, R) * u_N0, axis=1, keepdims = True) #Nx1
+                f1 = np.mean(psi * np.reshape(c_N0, [1,N,1,1]), axis=0) 
+                f2 = np.mean(np.reshape(np.tensordot(psi, theta, axes=([2,3],[0,1])), [N1,N,1,1])* psi, axis=0)
+                f3 = np.sum(np.reshape(np.tensordot(psi-bar_psi, theta, axes=([2,3],[0,1])), [N1,N,1,1])* (psi-bar_psi), axis=0) / (N1-1)
+                critic_grad = np.mean(f1+f2-f3, axis=0)
+                
+                natural_grad = np.matmul(theta[d:,:], np.concatenate((-np.identity(d),K), axis=0))
+                # when T is large, the first few steps are inefficient because it is too small
+                if 2 * t > T: 
+                    # actor update
+                    K -= beta * natural_grad
+                    # critic update
+                    theta -= alpha * critic_grad
+                else: #boost speed at the beginning
+                    # actor update
+                    K -= 3 * beta * natural_grad
+                    # critic update
+                    theta -= 3 * alpha * critic_grad
+            train_history = np.array(train_history)
+            print('errors', train_history[-1,1:])
+            if index == 0:
+                saved_history = train_history
+            else:
+                saved_history = np.concatenate((saved_history, train_history), axis=1)
+        # saved_history has size 
+        critic = saved_history[:,1:4*num_run:4]
+        actor = saved_history[:,2:4*num_run:4]
+        critic_mean = np.mean(np.log10(critic), axis=1, keepdims=True)
+        actor_mean = np.mean(np.log10(actor), axis=1, keepdims=True)
+        critic_std = np.std(np.log10(critic), axis=1, keepdims=True)
+        actor_std = np.std(np.log10(actor), axis=1, keepdims=True)
+        error_size = np.concatenate((critic_mean, actor_mean, critic_std, actor_std), axis=1)
+        if size == 0:
+            saved_history_concat = error_size
+        else:
+            saved_history_concat = np.concatenate((saved_history_concat, error_size), axis=1)
         
-        # compute the max norms in order to check assumption 1
-        norm_A_BK = np.linalg.norm(A_BK, ord=2)
-        E = np.matmul(np.concatenate((np.identity(d),-K), axis=0), np.concatenate((A,B), axis=1))
-        norm_E = np.linalg.norm(E, ord=2)
-        norm_K = np.linalg.norm(K)
-        norm_theta = np.linalg.norm(theta)
-        max_norm = np.maximum(max_norm, [norm_A_BK, norm_E, norm_K, norm_theta])
-        
-        # critic
-        # observe N0 trajectories
-        x_N0 = np.zeros([N, d]) #Nxd
-        for _ in range(N0):
-            x_N0 = np.tensordot(x_N0, A_BK, axes=([1],[1]))
-            x_N0 += np.reshape(normal.rvs(mean=np.zeros([d]), cov=D_ep, size=(N)), [N,d])
-        u_N0 = - np.tensordot(x_N0, K, axes=([1],[1])) + sigma**2 * np.reshape(normal.rvs(size=[N,k]), [N,k]) #Nxk
-        xu_N0 = np.concatenate((x_N0, u_N0), axis=1)
-        
-        # observe hat_psi_j
-        x_prime = np.tile(np.tensordot(x_N0, A, axes=([1],[1])), (N1,1,1)) #N1xNxd
-        x_prime += np.tile(np.tensordot(u_N0, B, axes=([1],[1])), (N1,1,1)) #N1xNxd
-        x_prime += np.reshape(normal.rvs(mean=np.zeros([d]), cov=D_xi, size=(N1, N)), [N1,N,d]) #N1xNxd
-        u_prime = - np.tensordot(x_prime, K, axes=([2],[1])) + sigma**2 * np.reshape(normal.rvs(size=[N1, N, k]), [N1, N, k])#N1xNxk
-        xu_prime = np.concatenate((x_prime, u_prime), axis=2) #N1xNx(d+k)
-        phi_N0 = np.reshape(xu_N0, [N,d+k,1]) * np.reshape(xu_N0, [N,1,d+k])#Nx(d+k)x(d+k)
-        psi = np.reshape(xu_prime, [N1,N,d+k,1]) * np.reshape(xu_prime, [N1,N,1,d+k]) - np.reshape(phi_N0, [1,N,d+k,d+k]) #N1xNx(d+k)x(d+k)
-        
-        # compute critic gradient
-        bar_psi = np.mean(psi, axis=0, keepdims=True) #1xNx(d+k)x(d+k)
-        c_N0 = np.sum(np.matmul(x_N0, Q) * x_N0, axis=1, keepdims = True)\
-            + np.sum(np.matmul(u_N0, R) * u_N0, axis=1, keepdims = True) #Nx1
-        f1 = np.mean(psi * np.reshape(c_N0, [1,N,1,1]), axis=0) 
-        f2 = np.mean(np.reshape(np.tensordot(psi, theta, axes=([2,3],[0,1])), [N1,N,1,1])* psi, axis=0)
-        f3 = np.sum(np.reshape(np.tensordot(psi-bar_psi, theta, axes=([2,3],[0,1])), [N1,N,1,1])* (psi-bar_psi), axis=0) / (N1-1)
-        critic_grad = np.mean(f1+f2-f3, axis=0)
-        
-        # actor update
-        natural_grad = np.matmul(theta[d:,:], np.concatenate((-np.identity(d),K), axis=0))
-        K -= beta * natural_grad
-        
-        # critic update
-        theta -= alpha * critic_grad
-    
-    train_history.append(max_norm)
-    # plt.plot(np.log10(train_history[:,1]))
-    # plt.plot(np.log10(train_history[:,2]))
-    print('max norm', max_norm)
-    np.savetxt('{}.csv'.format(FLAGS.config_path),
-                train_history,
-                fmt=['%.5e', '%.5e', '%.5e', '%.5e'],
+    np.savetxt('{}.csv'.format(FLAGS.exp_name),
+                saved_history_concat,
+                fmt=['%.5e', '%.5e', '%.5e', '%.5e'] * 6,
                 delimiter=",",
-                header='step, err_critic, err_actor, err_K',
+                # header='step, err_critic, err_actor, err_K,'*(num_run-1) + 'step, err_critic, err_actor, err_K',
                 )
-    
+    np.savetxt('{}.txt'.format(FLAGS.exp_name + 'norm'), max_norm)
 if __name__ == '__main__':
     app.run(main)
